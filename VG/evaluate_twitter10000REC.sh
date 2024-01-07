77dec2da5667ac28d2e3f2b7fd23ec511a73d6a7
Jinyuan Li <113409361+JinYuanLi0012@users.noreply.github.com>
2024-01-07 18:37:37 +0800

    Add files via upload

---
+++ VG/evaluate_twitter10000REC.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# The port for communication. Note that if you want to run multiple tasks on the same machine,
+# you need to specify different port numbers.
+export MASTER_PORT=6081
+export PYTHONPATH=$PYTHONPATH:.../OFA/fairseq
+
+
+user_dir=../../ofa_module
+bpe_dir=../../utils/BPE
+selected_cols=0,4,2,3
+
+
+########################## Evaluate Refcocog ##########################
+data=../../dataset/refcoco_data/twitter10000REC_addent_test_pred.tsv
+path=.../OFA/run_scripts/refcoco/twitter10000REC_large_checkpoints/20_3e-5_512/checkpoint.best_score_0.7390.pt
+result_path=../../results/twitter10000REC/OFAlargeVE_OFAlargeREC_pred
+split='refcoco_val'
+CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --use_env --master_port=${MASTER_PORT} ../../evaluate.py \
+    ${data} \
+    --path=${path} \
+    --user-dir=${user_dir} \
+    --task=refcoco \
+    --batch-size=32 \
+    --log-format=simple --log-interval=10 \
+    --seed=7 \
+    --gen-subset=${split} \
+    --results-path=${result_path} \
+    --beam=5 \
+    --min-len=4 \
+    --max-len-a=0 \
+    --max-len-b=4 \
+    --no-repeat-ngram-size=3 \
+    --fp16 \
+    --num-workers=0 \
+    --model-overrides="{\"data\":\"${data}\",\"bpe_dir\":\"${bpe_dir}\",\"selected_cols\":\"${selected_cols}\"}"
